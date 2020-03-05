d32b79d1261b5fc43ac19c8c791d84aa648bfe8d
Queejon <queejonjin@gmail.com>
2020-03-04 23:11:33 -0600

    Initial Commit

---
+++ public/electron.js
@@ -0,0 +1,70 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+//const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+function loadMenu(){
+  const menuTemp = [
+    {
+      label: 'Home',
+      click: () => {
+        
+      }
+    },
+    {
+      label: 'Log in',
+      click: () => {
+        
+      }
+    },
+    {
+      label: 'About',
+      click: () => {
+        
+      }
+    },
+    {
+      label: 'Profile',
+      click: () => {
+        
+      }
+    },
+    {
+      label: 'Map',
+      click: () => {
+        
+      }
+    },
+    {
+      label: 'Actions',
+      click: () => {
+        
+      }
+    }
+  ]
+  const menu = electron.Menu.buildFromTemplate(menuTemp);
+  electron.Menu.setApplicationMenu(menu);
+}
+
+app.whenReady().then(createWindow).then(loadMenu)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
