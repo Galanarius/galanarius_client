e7dc407f234a53fd952863bf9015d58946e7f9be
Queejon <queejonjin@gmail.com>
2020-03-06 23:19:48 -0600

    Failed Buttons

--- src/App.js
+++ src/App.js
@@ -1,96 +1,158 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App{
-  constructor(){
+class App extends React.Component{
+  constructor(props){
+    super(props);
     this.state = {
-      page_id: 'home',
-      discord_id: '-1',
-      
+      discord_id: "-1",
+      profile: {},
+        page_id: "home",
+        page: this.Home(),
     }
   }
-  App(){
+
+  tick(){
+    this.render();
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render(){
     return(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-          <p className="Home-title">
-            { this.Page('home')}
+          <p>
+            {this.Home()}
           </p>
         </header>
       </div>
     );
   }
-  
-  Page(type){
-    let pages = {
-      "home": this.Home(),
-      "login": this.Login(),
-      "about": this.About(),
-      "profile": this.Profile(),
-      "maps": this.Maps(),
-      "actions": this.Actions(),
-    }
-      return pages[type];
+
+  setPage_home(props){
+    this.setState({page: this.Home()});
+  }
+
+  setPage_login(props){
+    this.setState({page: this.Login()});
+  }
+
+  setPage_about(props){
+    this.setState({page: this.About()});
+  }
+
+  setPage_profile(props){
+    this.setState({page: this.Profile()});
+  }
+
+  setPage_maps(props){
+    this.setState({page: this.Maps()});
+  }
+
+  setPage_actions(props){
+    this.setState({page: this.Actions()});
   }
   
-  Home(){
+  Home(props){
     return(
       <div>
         <p>
-          Home
+          <div className="home">
+            Home
+          </div>
+          <button type="button"className="tabs" id="login_button" onclick={this.setPage_login}>Log In</button>
+          <button type="button"className="tabs" id="about_button" onclick={this.setPage_about}>About</button>
+          <button type="button"className="tabs" id="profile_button" onclick={this.setPage_profile}>Profile</button>
+          <button type="button"className="tabs" id="maps_button" onclick={this.setPage_maps}>Maps</button>
+          <button type="button"className="tabs" id="actions_button" onclick={this.setPage_actions}>Actions</button>
         </p>
       </div>
     );
   }
   
-  Login(){
+  Login(props){
     return(
       <div>
-        <p>
+        <p className="login">
           Log In
         </p>
+          <button type="button"className="tabs-home" id="home_button" onclick={this.setPage_home}>Home</button>
+          <button type="button"className="tabs" id="about_button" onclick={this.setPage_about}>About</button>
+          <button type="button"className="tabs" id="profile_button" onclick={this.setPage_profile}>Profile</button>
+          <button type="button"className="tabs" id="maps_button" onclick={this.setPage_maps}>Maps</button>
+          <button type="button"className="tabs" id="actions_button" onclick={this.setPage_actions}>Actions</button>
       </div>
     );
   }
   
-  About(){
+  About(props){
     return(
       <div>
-        <p>
+        <p className="about">
           About
         </p>
+          <button type="button"className="tabs" id="login_button" onclick={this.setPage_home}>Log In</button>
+          <button type="button"className="tabs-home" id="home_button" onclick={this.setPage_login}>Home</button>
+          <button type="button"className="tabs" id="profile_button" onclick={this.setPage_profile}>Profile</button>
+          <button type="button"className="tabs" id="maps_button" onclick={this.setPage_maps}>Maps</button>
+          <button type="button"className="tabs" id="actions_button" onclick={this.setPage_actions}>Actions</button>
       </div>
     );
   }
   
-  Profile(){
+  Profile(props){
     return(
       <div>
-        <p>
+        <p className="profile">
           Profile
         </p>
+          <button type="button"className="tabs" id="login_button" onclick={this.setPage_home}>Log In</button>
+          <button type="button"className="tabs" id="about_button" onclick={this.setPage_about}>About</button>
+          <button type="button"className="tabs-home" id="home_button" onclick={this.setPage_login}>Home</button>
+          <button type="button"className="tabs" id="maps_button" onclick={this.setPage_maps}>Maps</button>
+          <button type="button"className="tabs" id="actions_button" onclick={this.setPage_actions}>Actions</button>
       </div>
     );
   }
   
-  Maps(){
+  Maps(props){
     return(
       <div>
-        <p>
-          Map
+        <p className="maps">
+          Maps
         </p>
+          <button type="button"className="tabs" id="login_button" onclick={this.setPage_home}>Log In</button>
+          <button type="button"className="tabs" id="about_button" onclick={this.setPage_about}>About</button>
+          <button type="button"className="tabs" id="profile_button" onclick={this.setPage_profile}>Profile</button>
+          <button type="button"className="tabs-home" id="home_button" onclick={this.setPage_login}>Home</button>
+          <button type="button"className="tabs" id="actions_button" onclick={this.setPage_actions}>Actions</button>
       </div>
     );
   }
   
-  Actions(){
+  Actions(props){
     return(
       <div>
-        <p>
+        <p className="actions">
           Actions
         </p>
+          <button type="button"className="tabs" id="login_button" onclick={this.setPage_home}>Log In</button>
+          <button type="button"className="tabs" id="about_button" onclick={this.setPage_about}>About</button>
+          <button type="button"className="tabs" id="profile_button" onclick={this.setPage_profile}>Profile</button>
+          <button type="button"className="tabs" id="maps_button" onclick={this.setPage_maps}>Maps</button>
+          <button type="button"className="tabs-home" id="home_button" onclick={this.setPage_login}>Home</button>
       </div>
     );
   }
@@ -98,7 +160,7 @@ class App{
   /*GalanERR(){
     return(
       <div>
-        <p>
+        <p className="error">
           ERR
         </p>
       </div>
